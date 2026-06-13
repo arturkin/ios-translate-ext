b1c59c313670affa69a23957b52acdb488ad6a7b
arturkin <i@arturkin.com>
2026-06-13 17:28:33 +0000

    Initial Commit

---
+++ Translate Icelandic/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Translate Icelandic
+//
+//  Created by Artur Alekseev on 13.6.2026.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
